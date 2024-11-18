8b9f6b97b055fae25ba82c64e20677f1becb99e5
kristofmoldovan <moldovan.kristof@gmail.com>
2024-11-18 13:50:15 +0100

    switching to 512x512x32, fp16

--- dataset_dataloader.py
+++ dataset_dataloader.py
@@ -84,7 +84,7 @@ class LungsDataset(Dataset):
         img = self.pad_array(img, target_shape)
         mask = self.pad_array(mask, target_shape)
 
-        img = img[0:256, 0:256, 0:32] # [:, :, 70:78]
+        img = img[0:512, 0:512, 0:32] # [:, :, 70:78]
         mask = mask[0:256, 0:256, 0:32]
 
         img = np.expand_dims(img, axis=0)

9a62d4995fe32b4c64e2c9791ff88570ae1bf869
kristofmoldovan <moldovan.kristof@gmail.com>
2024-11-28 22:43:25 +0100

    less verbose debug

--- loss_metric.py
+++ loss_metric.py
@@ -163,13 +163,6 @@ class BCEDiceLoss(nn.Module):
         print("BCE LOSS", bce_loss)
         print("DICE LOSS", dice_loss)
 
-        with torch.no_grad():
-            print(logits)
-            print(targets)
-
-            print(torch.unique(targets))
-            print(torch.unique())
-
         return bce_loss + dice_loss
 
 class BCEDiceLossMiddle(nn.Module):
